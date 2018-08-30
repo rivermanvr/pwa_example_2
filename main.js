2587d13fdb321d51a5417f5a663fc89fd92592c7
rivermanvr <vrgithub@riversconsulting.com>
2018-08-30 16:50:29 -0400

    Registration Object - Example 1 finished

--- main.js
+++ main.js
@@ -4,8 +4,12 @@
 if (navigator.serviceWorker) {
 
   // Register the SW
-  navigator.serviceWorker.register('/sw.js', {scope: '/posts/'})
+  navigator.serviceWorker.register('/sw.js')
     .then(function(registration){
+
       console.log('SW Registered');
+      //comment out later....
+      console.log(registration);
+
     }).catch(console.log);
 }
--- sw.js
+++ sw.js
@@ -1,5 +1,15 @@
 
 // Service Worker
+
+// commented out this waitUntil code.
+// This will be used in the registration object section   --->  Example 1
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(new Promise (resolve => {
+    setTimeout(resolve, 5000);
+  }))
+});
+
 self.addEventListener('activate', () => {
   console.log('SW: Active');
 });
