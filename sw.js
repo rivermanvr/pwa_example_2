f42c64bf2af98b36b326db220e3e894d1579f46d
rivermanvr <vrgithub@riversconsulting.com>
2018-08-31 14:43:57 -0400

    Commenting out waitUntil method in sw.js to reset this file

--- sw.js
+++ sw.js
@@ -4,11 +4,11 @@
 // commented out this waitUntil code.
 // This will be used in the registration object section   --->  Example 1
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(new Promise (resolve => {
-    setTimeout(resolve, 5000);
-  }))
-});
+// self.addEventListener('install', (e) => {
+//   e.waitUntil(new Promise (resolve => {
+//     setTimeout(resolve, 5000);
+//   }))
+// });
 
 self.addEventListener('activate', () => {
   console.log('SW: Active');
